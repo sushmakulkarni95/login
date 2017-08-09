e04edcd36d4eb370048e145b7149301317a6f722
sushmakulkarni95 <30827288+sushmakulkarni95@users.noreply.github.com>
2017-08-09 13:38:49 +0530

    Update login.ts

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -24,26 +24,7 @@ export class LoginPage {
 
   }
 
-         private deviceDetails = "";
-    populateDeviceDetails() {
-        var device:Device = (<any>window).device;
-        
-        
-        this.deviceDetails = "";
-        this.deviceDetails += "<br/>Cordova:" + device.cordova;
-        this.deviceDetails += "<br/>model:" + device.model;
-        this.deviceDetails += "<br/>platform:" + device.platform;
-        this.deviceDetails += "<br/>uuid:" + device.uuid;
-        this.deviceDetails += "<br/>version:" + device.version;
-        this.deviceDetails +=
-        "<br/>manufacturer:" + device.manufacturer;
-        this.deviceDetails += "<br/>isVirtual:" + device.isVirtual;
-        this.deviceDetails += "<br/>serial:" + device.serial;
-
-
-  
-  
-    }
+    
 }
   
      
